eb85a5e4aa98c09caab293309dea45ed9f315ee2
Eduardo Pereira <145592635+AldebaraFork@users.noreply.github.com>
2024-08-28 18:44:18 -0300

    ContaFisica feature complete
    workin on tests soon

---
+++ BankSystem3/src/ContaFisica.java
@@ -0,0 +1,56 @@
+public class ContaFisica extends Conta implements ValidaSenha {
+
+    //CONSTRUTOR DE CONTAS FISICAS, TODA CONTA FISICA DEVE POSSUIR NOME,EMAIL E IDADE
+    public ContaFisica() {
+        setNome("");
+        setEmail("");
+        setIdade(0);
+    }
+
+    //PROPRIEDADES DA CONTA FISICA
+    private String senha;
+    private String cpf;
+
+    @Override
+    public String getSenha() {
+        return senha;
+    }
+
+    @Override
+    public void setSenha(String senha) {
+        try {
+            System.out.println("Insira a senha: ");
+            senha = ler.nextLine();
+            if (senha.length() < 8) {
+                System.out.println("Senha invalida! Tente novamente");
+                setSenha("");
+            } else {
+                System.out.println("Senha salva! ");
+                this.senha = senha;
+
+            }
+        } catch (Exception e) {
+            System.out.println("Ocorreu o erro " + e.getMessage() + "Tente novamente");
+            setSenha("");
+        }
+
+
+    }
+
+    @Override
+    public void validaSenha(String senha) {
+      try {
+          System.out.println("Insira a senha:");
+          senha = ler.nextLine();
+          if(senha.equals(this.senha)) {
+              System.out.println("Senha validada com sucesso! ");
+          }else {
+              System.out.println("Senha invalida! Tente novamente");
+              validaSenha("");
+          }
+      }catch (Exception e) {
+          System.out.println("Ocorreu o erro: " +  e.getMessage());
+          System.out.println("Causa: " + e.getCause() + ", Tente novamente");
+      }
+    }
+}
--- BankSystem3/src/ValidaSenha.java
+++ BankSystem3/src/ValidaSenha.java
@@ -1,8 +1,9 @@
 public interface ValidaSenha {
 
+    //AS CONTAS DEVEM POSSUIR UMA SENHA, SEJA FISICA OU JURIDICA
     public void setSenha(String senha);
 
+    public void validaSenha(String senha);
 
-
-    public String validaSenha();
+    public String getSenha();
 }
