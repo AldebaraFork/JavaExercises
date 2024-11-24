5ac65edc6d2aef701f6141813d110d8c263abb3a
Eduardo Pereira <145592635+AldebaraFork@users.noreply.github.com>
2024-11-24 12:56:14 -0300

    New features
    -adicionado saque e deposito (em testes)
    -ao criar uma conta automaticamente é tambem criado um cliente

--- DuduBank/src/Conta.java
+++ DuduBank/src/Conta.java
@@ -1,9 +1,102 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Conta {
 
     //PROPRIEDADES
-     Cliente titular;
-     int agencia;
-     int numero;
-     double saldo;
-     
+    Cliente titular;
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    Scanner ler = new Scanner(System.in);
+    Cliente cliente = new Cliente("", "", "", 0);
+
+
+    public Conta(){
+        this.titular = cliente;
+    }
+    //GETTERS
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    //SETTERS
+
+    public void setAgencia(int agencia) {
+        try {
+            System.out.println("Insira a agencia: ");
+            agencia = ler.nextInt();
+            if (agencia > 0) {
+                System.out.println("Agencia: " + agencia + " salva com sucesso");
+                this.agencia = agencia;
+            } else {
+                System.out.println("Agencia invalida! tente novamente");
+                setAgencia(0);
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Insira um numero de agencia valido! ");
+            setAgencia(0);
+        }
+    }
+
+    public void setNumero(int numero) {
+        try {
+            System.out.println("Insira o numero da conta: ");
+            numero = ler.nextInt();
+            if (numero > 0) {
+                System.out.println("Numero da conta: " + numero + " salvo com sucesso! ");
+                this.numero = numero;
+            } else {
+                System.out.println("Numero invalido! tente novamente");
+                setNumero(0);
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Insira um numero de agencia valido! ");
+            setNumero(0);
+        }
+    }
+
+    public void depositar(double valor) {
+        try {
+            System.out.println("Insira o valor para deposito: ");
+            valor = ler.nextDouble();
+            if (valor <= 0) {
+                System.out.println("Valor invalido! Tente novamente");
+                depositar(0);
+            } else {
+                System.out.println("Valor depositado com sucesso! ");
+                this.saldo += valor;
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Insira um valor valido para deposito! ");
+            depositar(0);
+        }
+    }
+
+    public void sacar(double valor) {
+        try {
+            System.out.println("Insira um valor para saque: ");
+            valor = ler.nextDouble();
+            if (valor > getSaldo()) {
+                System.out.println("Valor para saque invalido! Não há dinheiro suficiente");
+                sacar(0);
+            } else {
+                System.out.println("Saque de  R$:" + valor + " Autorizado! ");
+                this.saldo -= valor;
+            }
+        }catch (InputMismatchException e) {
+            System.out.println("Insira um valor valido para saque! ");
+            sacar(0);
+        }
+    }
 }
--- DuduBank/src/Main.java
+++ DuduBank/src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Cliente teste = new Cliente("","","",0);
 
-        System.out.println(teste.getNome());
-        System.out.println(teste.getCpf());
-        System.out.println(teste.getEmail());
-        System.out.println(teste.getIdade());
+
+        Conta conta = new Conta();
+
+
+
+        System.out.println(conta.titular.getNome());
+
+
     }
 }
